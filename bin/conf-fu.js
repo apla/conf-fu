16bd52ac6ad561e050b03609dc12ace0277f19c8
apla <dot.and.thing@gmail.com>
2014-09-15 20:35:01 +0400

    rewritten cli handling

--- bin/conf-fu.js
+++ bin/conf-fu.js
@@ -1,33 +1,9 @@
 #!/usr/bin/env node
 
-var path    = require ('path');
-var options = require ('minimist')(process.argv.slice(2), {
-	boolean: "verbose",
-	alias: {
-		verbose: "v"
-	}
-});
-
 var MODULE_NAME = 'conf-fu';
 var confFu      = require (MODULE_NAME + '/cli.js');
 
 var paint = confFu.paint;
-
-if (!options.configFile)
-	options.configFile = process.env.CONF_FU;
-
-if (!options.configFile) {
-	console.log (
-		paint.confFu (),
-		"please define core config file using CONF_FU environment variable or --config command line option"
-	);
-}
-
-if (!options.fixupFile)
-	options.fixupFile = process.env.CONF_FU_FIXUP;
-
-if (!options.instance)
-	options.instance = process.env.CONF_FU_INSTANCE || process.env.INSTANCE;
 
 var conf = new confFu (options);
 
--- cli.js
+++ cli.js
@@ -1,6 +1,7 @@
 var ConfFu = require ('./index');
 
 var paint = ConfFu.paint;
+var yargs = require ("yargs");
 
 function runEditor (fileName) {
 	var child_process = require ('child_process');
@@ -17,32 +18,121 @@ function runEditor (fileName) {
 }
 
 var cli = {
-	dump: function () {
-		console.log (JSON.stringify (this.config, null, "\t"));
-		return;
+	verbose: {
+		alias: "v",
+		boolean: true,
+		description: "verbose output",
+		default: false
 	},
-	vars: function () {
-		this.logVariables ();
+	druRun: {
+		alias: ["n", "dry-run"],
+		boolean: true,
+		description: "don't validate settings",
+		default: false
 	},
-	_: function () {
-		// this is a bit hackish function.
-		// if we found orphan parameters, we launch this function to set parameters
+	configFile: {
+		alias: ["config"],
+		description: "core config file to process",
+		env: "CONF_FU"
+	},
+	fixupFile: {
+		alias: ["fixup"],
+		description: "fixup config to apply on core",
+		env: "CONF_FU_FIXUP"
+	},
+	instance: {
+		description: "config instance name",
+		env: ["CONF_FU_INSTANCE", "INSTANCE"]
+	},
+	dump: {
+		description: "dump current config as JSON",
+		run: function () {
+			console.log (JSON.stringify (this.config, null, "\t"));
+			return;
+		}
+	},
+	vars: {
+		description: "show available variables",
+		run: function () {
+			this.logVariables ();
+		}
+	},
+	_: {
+		anyway: true, // launch anyway, even if error is present
+		help: "",
+		run: function (options) {
+			// this is a bit hackish function.
+			// if we found orphan parameters, we launch this function to set parameters
+			if (options._ && options._.length > 0 && options._.length % 2 === 0) {
 
+				var fixupVars = {};
+				options._.forEach (function (item, idx, arr) {
+					if (idx % 2)
+						return;
+					fixupVars[item] = [arr[idx + 1]];
+					console.log ('variable', item, 'assigned to the', arr[idx + 1]);
+				});
+				// console.log (fixupVars);
+				// fixupVars[context.varPath] = [context.value];
+				this.setVariables (fixupVars, true);
+			}
+
+		},
 	},
-	edit: function (options) {
-		if (options.edit === "fixup") {
-			runEditor (this.fixupFile.path);
-		} else if (options.edit === "core") {
-			runEditor (this.configFile.path);
+	edit: {
+		anyway: true, // launch anyway, even if error is present
+		description: "run default editor for config, --edit core for core and --edit fixup for fixup",
+		run: function (options) {
+			if (options.edit === "fixup") {
+				runEditor (this.fixupFile.path);
+			} else if (options.edit === "core") {
+				runEditor (this.configFile.path);
+			}
+		}
+	},
+	help: {
+		alias: "h",
+		boolean: true,
+		anyway: true, // launch anyway, even if error is present
+		before: true,
+		banner: paint.confFu() + " usage:",
+		run: function () {
+			console.log (cli.help.banner);
+			for (var paramName in cli) {
+				console.log (paint.path ("--"+paramName), cli[paramName].help);
+			}
 		}
 	}
 };
 
-cli.editAnyway = cli.edit;
 
-function onConfigReady (options) {
-	// options must be handled before variables setup
 
+function initOptions () {
+
+	yargs.usage (initOptions.cli.help.banner, initOptions.cli);
+	var options = yargs.parse (process.argv);
+
+	for (var k in initOptions.cli) {
+		if (!initOptions.cli[k].env)
+			continue;
+		if (options[k])
+			continue;
+
+		var envVars = initOptions.cli[k].env;
+		if (envVars.constructor !== Array)
+			envVars = [envVars];
+		envVars.forEach (function (envVar) {
+			if (process.env[envVar])
+				options[k] = process.env[envVar];
+		});
+	}
+
+	return options;
+}
+
+initOptions.cli = cli;
+
+function findCommand (options) {
 	var haveCommand;
 	var haveParams  = options._;
 	for (var k in options) {
@@ -54,65 +144,24 @@ function onConfigReady (options) {
 			].join (' and '));
 			return;
 		}
-		if (k !== '_') {
+		if (k !== '_' && cli[k].run) {
 			haveCommand = k;
 		}
 	}
+	return haveCommand;
+}
 
-	if (haveCommand) {
-		cli[haveCommand].apply (this, arguments);
-		return;
-	}
+function onConfigReady (options) {
+	var haveCommand = findCommand (options) || '_';
 
-	if (options._ && options._.length > 0 && options._.length % 2 === 0) {
-
-		var fixupVars = {};
-		options._.forEach (function (item, idx, arr) {
-			if (idx % 2)
-				return;
-			fixupVars[item] = [arr[idx + 1]];
-			console.log ('variable', item, 'assigned to the', arr[idx + 1]);
-		});
-		// console.log (fixupVars);
-		// fixupVars[context.varPath] = [context.value];
-		this.setVariables (fixupVars, true);
-	}
-
+	cli[haveCommand].run.apply (this, arguments);
 }
 
 function onConfigFail (options) {
-	var haveCommand;
-	var haveParams  = options._;
+	var haveCommand = findCommand (options) || '_';
 
-	for (var k in options) {
-		if (!(k+'Anyway' in cli))
-			continue;
-		if (haveCommand) {
-			console.error (paint.confFu (), 'you cannot launch two commands at once:', [
-				paint.path (k), paint.path (haveCommand)
-			].join (' and '));
-			return;
-		}
-		haveCommand = k+'Anyway';
-	}
-
-	if (haveCommand) {
-		cli[haveCommand].apply (this, arguments);
-		return;
-	}
-
-	if (options._ && options._.length > 0 && options._.length % 2 === 0) {
-		var fixupVars = {};
-		options._.forEach (function (item, idx, arr) {
-			if (idx % 2)
-				return;
-			fixupVars[item] = [arr[idx + 1]];
-			console.log ('variable', item, 'assigned to the', arr[idx + 1]);
-		});
-		// console.log (fixupVars);
-		// fixupVars[context.varPath] = [context.value];
-		this.setVariables (fixupVars, true);
-	}
+	if (cli[haveCommand].anyway)
+		cli[haveCommand].run.apply (this, arguments);
 }
 
 function onConfigError (options, eOrigin, eType, eData, eFile) {
@@ -134,9 +183,27 @@ function onConfigError (options, eOrigin, eType, eData, eFile) {
 }
 
 function ConfFuCLI (options) {
-	var conf = new ConfFu (options);
 
-	conf.cli = cli;
+	if (!options)
+		options = ConfFuCLI.initOptions ();
+
+	var haveCommand = findCommand (options);
+
+	if (!options.configFile) {
+		console.log (
+			paint.confFu (),
+			"please define core config file using CONF_FU environment variable or --config-file command line option"
+		);
+	}
+
+
+	if (haveCommand && cli[haveCommand].before) {
+		cli[haveCommand].run.apply (this, arguments);
+		if (options.dryRun)
+			return;
+	}
+
+	var conf = new ConfFu (options);
 
 	conf.on ('ready', onConfigReady.bind (conf, options));
 	conf.on ('notReady', onConfigFail.bind (conf, options));
@@ -144,6 +211,8 @@ function ConfFuCLI (options) {
 
 	return conf;
 }
+
+ConfFuCLI.initOptions = initOptions;
 
 for (var k in ConfFu) {
 	ConfFuCLI[k] = ConfFu[k];
